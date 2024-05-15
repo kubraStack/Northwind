8c913ced1dc2a003e158c95aea7b2fabd6356c9b
kubrastack <kmrt3014@gmail.com>
2024-05-16 01:31:05 +0300

    add basic layouts

--- src/app/routes/Home-Page/Home-Page.component.ts
+++ src/app/routes/Home-Page/Home-Page.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BasicLayoutComponent } from '../../shared/components/basic-layout/basic-layout.component';
 
 @Component({
     selector: 'app-home-page',
     standalone: true,
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        BasicLayoutComponent
+
     ],
     templateUrl: './Home-Page.component.html',
     styleUrl: './Home-Page.component.scss',
